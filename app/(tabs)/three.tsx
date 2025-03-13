dea5ef8109e9ac15f77e0fd7722dd99c4fde48e8
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-03-13 10:43:46 +0100

    début du projet, initialisation stack-navigator + 3 pages

--- app/(tabs)/three.tsx
+++ app/(tabs)/three.tsx
@@ -2,13 +2,13 @@ import {StyleSheet} from 'react-native';
 import {Text, View} from '@/components/Themed';
 import {Link} from "expo-router";
 
-export default function TabOneScreen() {
+export default function TabThreeScreen() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Tab One</Text>
+            <Text style={styles.title}>Tab Three</Text>
             <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
             <Text style={styles.title}>
-                <Link href="/two">Go to Page 2</Link>
+                <Link href="/">Back Home</Link>
             </Text>
         </View>
     );
